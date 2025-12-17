1c5a3f2e58c1b72569ee7e563bf7bb5198dedbf7
arsacahaya <arsacahaya.upnvj@gmail.com>
2025-12-17 15:06:03 +0700

    fix: SSH key setup (new)

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -53,11 +53,7 @@
 <footer class="bg-white rounded shadow p-4 mb-4 mt-4">
     <div class="row">
         <div class="col-12 col-md-4 col-xl-6 mb-4 mb-md-0">
-<<<<<<< HEAD
-            <p class="mb-0 text-center text-lg-start">© 2019-<span class="current-year"></span>
-=======
             <p class="mb-0 text-center text-lg-start">© <span class="current-year"></span>
->>>>>>> 4e6d9f2e2e3c9d56c03a6b917c96f3b2cae2a5b8
             DWO Kelompok 3
             </p>
         </div>
